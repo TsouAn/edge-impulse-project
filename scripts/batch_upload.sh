03d51906cf132f77b8127c77313803353d6587b6
Haha <413400671@o365.tku.edu.tw>
2025-12-17 09:16:23 +0800

    added batch_upload.sh

--- scripts/batch_upload.sh
+++ scripts/batch_upload.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-DATA_DIR="$1"
+DATA_DIR="$1"git
 for label_dir in "$DATA_DIR"/*; do
     LABEL=$(basename "$label_dir")
     ./scripts/upload_data.sh "$LABEL" "$label_dir"/*.{jpg,jpeg,png,bmp}
